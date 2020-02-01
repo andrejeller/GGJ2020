0f056657355a730cdfe240d062da449399ebd1d0
Felipe Silva "Hitsu <felipe.silva.hts@gmail.com>
2020-02-01 12:24:15 -0300

    progress bar

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Image m_progressBar;
+    private float m_progressPercent = 0.3f;
+    public Text PercentProgres;
+    
+    
+    void Start() {
+        m_progressBar.fillAmount = m_progressPercent;
+    }
+
+    
+    void Update()
+    {
+
+        m_progressBar.fillAmount = m_progressPercent;
+        m_progressPercent -= Time.deltaTime / 30;
+        PercentProgres.text = (m_progressPercent*100).ToString("00");
+        
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            
+            m_progressPercent += 0.02f;
+
+            var rand = Random.Range(0, 100);
+            
+            if (rand > 0 && rand <= 1)
+            {
+                Debug.Log("CRASHPU");
+            }
+        }
+    }
+}
